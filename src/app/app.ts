ba444dfb931f272198d372fc681df78fa0bd768f
zzhirkovaa <162570251+zzhirkovaa@users.noreply.github.com>
2026-02-22 11:47:51 +0300

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  template: '<router-outlet/>САЙТ РАБОТАЕТ',
+})
+export class App {
+}
